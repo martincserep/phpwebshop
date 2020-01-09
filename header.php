e9f2fd02b68e0e697708118bcfaa57c06e2a37e4
Martin Cserép <martincserep1@gmail.com>
2020-01-09 12:57:15 +0100

    added delete and search but search has bug

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
     <!-- Latest compiled and minified Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
 
+
     <!-- our custom CSS
  <link rel="stylesheet" href="libs/css/custom.css" />
  -->
