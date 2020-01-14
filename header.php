3d30004bfd53b3c8cffa3613a616a1df0b0c41df
Martin Cserép <martincserep1@gmail.com>
2020-01-14 18:05:06 +0100

    customer nav is done

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
     <link href="<?php echo $home_url . "libs/css/admin.css" ?>" rel="stylesheet" />
     <link href="<?php echo $home_url . "libs/css/customer.css" ?>" rel="stylesheet" />
     <link href="<?php echo $home_url . "libs/css/main.css" ?>" rel="stylesheet" />
+    <link href="<?php echo $home_url . "libs/css/header.css" ?>" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
