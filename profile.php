40320073194ce7ee1a6bbc2bc32b70c8734a9556
Martin Cserép <martincserep1@gmail.com>
2020-01-16 00:42:24 +0100

    profile design is done

--- profile.php
+++ profile.php
@@ -23,51 +23,42 @@ $user_srv = new SimpleUserServices();
 
 $user = $user_srv->ReadUserById($user_id);
 ?>
-    <div class="form-horizontal list-border-background width-60-percent" role="form">
-        <form action="update_user_data.php?id=<?= $user_id ?>" method="post" enctype="multipart/form-data">
-            <div class="form-group">
-                <label class="col-lg-3 control-label">First name:</label>
-                <div class="col-lg-8">
+    <div class="middle-container" role="form">
+        <form class="update-profile" action="update_user_data.php?id=<?= $user_id ?>" method="post" enctype="multipart/form-data">
+                <label class="customer-label">First name:</label>
+                <div class="profile-form-item">
                     <input type="text" name="firstName" class="form-control" value="<?= $user->getFirstName() ?>"
                            required>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-lg-3 control-label">Last name:</label>
-                <div class="col-lg-8">
+                <label class="customer-label">Last name:</label>
+                <div class="profile-form-item">
                     <input type="text" name="lastName" class="form-control" value="<?= $user->getLastName() ?>"
                            required>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-lg-3 control-label">Contact number:</label>
-                <div class="col-lg-8">
+                <label class="customer-label">Contact number:</label>
+                <div class="profile-form-item">
                     <input type="text" name="contactNumber" class="form-control"
                            value="<?= $user->getContactNumber() ?>" required>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-lg-3 control-label">Email:</label>
-                <div class="col-lg-8">
+                <label class="customer-label">Email:</label>
+                <div class="profile-form-item">
                     <input type="email" name="email" class="form-control" value="<?= $user->getEmail() ?>" required>
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="col-md-3 control-label">Address:</label>
-                <div class="col-md-8">
+
+                <label class="customer-label">Address:</label>
+                <div class="profile-form-item">
                     <input type="text" name="address" class="form-control" value="<?= $user->getAddress() ?>" required>
                 </div>
-            </div>
             <br><br>
             <div class="mdi-format-align-middle">
-                <button type="submit" class="btn btn-info">
+                <button type="submit" class="button">
                     Update your details
                 </button>
                 <a href="change_password.php?user_id=<?= $user_id ?>" class="btn btn-info">
                     Change password
                 </a>
-                <a href="index.php" class="btn btn-success">
-                    <i class="fas fa-arrow-left"></i>
+                <a href="index.php" class="cancel">
+                   Cancel
                 </a>
             </div>
         </form>
